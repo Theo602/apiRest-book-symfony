f9f84d5b996c884dca98d3d097602e0d035ae44a
Theo602 <110599214+Theo602@users.noreply.github.com>
2023-03-07 22:10:29 +0100

    documentation en cours

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -20,9 +20,31 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 class AuthorController extends AbstractController
 {
+
+    /**
+     * Cette méthode permet de récupérer l'ensemble des auteurs. 
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne la liste des auteurs",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Book::class, groups={"getAuthor"}))
+     *     )
+     * )
+     * 
+     * @OA\Tag(name="Author")
+     * 
+     * @param AuthorRepository $authorRepository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/author', name: 'author', methods: ['GET'])]
     public function getAllAuthor(AuthorRepository $authorRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cachePool): JsonResponse
     {
@@ -42,6 +64,24 @@ class AuthorController extends AbstractController
         return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * Cette méthode permet de récupérer un auteur en particulier en fonction de son id. 
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne un auteur",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Book::class, groups={"getAuthor"}))
+     *     )
+     * )
+     * 
+     * @OA\Tag(name="Author")
+     *  
+     * @param Author $author
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/author/{id}', name: 'detailsAuthor', methods: ['GET'])]
     public function getDetailAuthor(Author $author, SerializerInterface $serializer): JsonResponse
     {
@@ -50,6 +90,20 @@ class AuthorController extends AbstractController
         return new JsonResponse($jsonAuthor, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
+    /**
+     * Cette méthode supprime un auteur en fonction de son id. 
+     * 
+     * @OA\Response(
+     *     response=204,
+     *     description="Supprime un auteur"
+     * )
+     * 
+     * @OA\Tag(name="Author")
+     * 
+     * @param Author $author
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
     #[Route('/api/author/delete/{id}', name: 'deleteAuthor', methods: ['DELETE'])]
     public function deleteAuthor(Author $author, EntityManagerInterface $em, TagAwareCacheInterface $cachePool): JsonResponse
     {
@@ -63,6 +117,39 @@ class AuthorController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * Cette méthode permet de Créer un nouvel auteur. 
+     *
+     *@OA\RequestBody(
+     *     description="créer un auteur",
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema (
+     *              type="object",
+     *              @OA\Property(property="firstname", description="Le prénom de l'auteur", type="string"),
+     *              @OA\Property(property="lastname", description="Le nom de l'auteur", type="string"),
+     *        )
+     *     )
+     *)
+
+     * @OA\Response(
+     *     response=201,
+     *     description="Créer un auteur",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Book::class, groups={"getAuthor"}))
+     *     )
+     * ) 
+     *  
+     * @OA\Tag(name="Author")
+     *     
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $em
+     * @param UrlGeneratorInterface $urlGenerator
+     * @return JsonResponse
+     */
     #[Route('/api/author/create', name: 'createAuthor', methods: ['POST'])]
     public function createAuthor(EntityManagerInterface $em, Request $request, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator, TagAwareCacheInterface $cachePool): JsonResponse
     {
@@ -89,6 +176,34 @@ class AuthorController extends AbstractController
         return new JsonResponse($jsonAuthor, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
+    /**
+     * Cette méthode permet de mettre à jour un auteur.
+     *  
+     *@OA\RequestBody(
+     *     description="Modifié un auteur",
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema (
+     *              type="object",
+     *              @OA\Property(property="firstname", description="Le prénom de l'auteur", type="string"),
+     *              @OA\Property(property="lastname", description="Le nom de l'auteur", type="string"),
+     *        )
+     *     )
+     *)
+     * 
+     * @OA\Response(
+     *     response=204,
+     *     description="Modifié un auteur"
+     * ) 
+     * @OA\Tag(name="Author")
+     * 
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param Author $currentAuthor
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
     #[Route('/api/author/edit/{id}', name: 'editAuthor', methods: ['PUT'])]
     public function editAuthor(EntityManagerInterface $em, Request $request, SerializerInterface $serializer, Author $currentAuthor, TagAwareCacheInterface $cachePool, ValidatorInterface $validator): JsonResponse
     {
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -21,9 +21,43 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 class BookController extends AbstractController
 {
+    /**
+     * Cette méthode permet de récupérer l'ensemble des livres.
+     *
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne la liste des livres",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Book::class, groups={"getBooks"}))
+     *     )
+     * )
+     * @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="La page que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     *
+     * @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Le nombre d'éléments que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     * )
+     * @OA\Tag(name="Books")
+     *
+     * @param BookRepository $bookRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     */
     #[Route('/api/books', name: 'book', methods: ['GET'])]
     public function getAllBook(BookRepository $bookRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cachePool, VersioningService $versioningService): JsonResponse
     {
@@ -32,8 +66,6 @@ class BookController extends AbstractController
         $limit = $request->get('limit', 3);
 
         $idCache = "getAllBook-" . $page . "-" . $limit;
-
-        
 
         $jsonBookList = $cachePool->get($idCache, function (ItemInterface $item) use ($bookRepository, $page, $limit, $serializer, $versioningService) {
             $item->tag("bookCache");
@@ -50,6 +82,23 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * Cette méthode permet de récupérer un livre en particulier en fonction de son id. 
+     * @OA\Response(
+     *     response=200,
+     *     description="Retourne un livre",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Book::class, groups={"getBooks"}))
+     *     )
+     * )
+     * 
+     * @OA\Tag(name="Books")
+     * 
+     * @param Book $book
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
     #[Route('/api/books/{id}', name: 'detailsBook', methods: ['GET'])]
     public function getDetailBook(Book $book, SerializerInterface $serializer, VersioningService $versioningService): JsonResponse
     {
@@ -60,6 +109,20 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
+    /**
+     * Cette méthode permet de supprimer un livre par rapport à son id. 
+     *          
+     * @OA\Response(
+     *     response=204,
+     *     description="Supprime un livre"
+     * ) 
+     * 
+     * * @OA\Tag(name="Books")
+     * 
+     * @param Book $book
+     * @param EntityManagerInterface $em
+     * @return JsonResponse 
+     */
     #[Route('/api/books/delete/{id}', name: 'deleteBook', methods: ['DELETE'])]
     public function deleteBook(Book $book, EntityManagerInterface $em, TagAwareCacheInterface $cachePool): JsonResponse
     {
@@ -72,6 +135,40 @@ class BookController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * Cette méthode permet d'insérer un nouveau livre.  
+     * 
+     * 
+     *@OA\RequestBody(
+     *     description="Créer un livre",
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema (
+     *              type="object",
+     *              @OA\Property(property="title", description="Le titre du livre", type="string"),
+     *              @OA\Property(property="coverText", description="La couverture du livre", type="string"),
+     *              @OA\Property(property="idAuthor", description="Author", type="int"),
+     *              @OA\Property(property="comment", description="Le commentaire du bibliothéquaire", type="string")
+     *        )
+     *     )
+     *)
+     * 
+     * @OA\Response(
+     *     response=201,
+     *     description="Créer un livre",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Book::class, groups={"getBooks"}))
+     *     )
+     * ) 
+     * 
+     * @OA\Tag(name="Books")
+     * 
+     * @param Book $book
+     * @param EntityManagerInterface $em
+     * @return JsonResponse 
+     */
     #[Route('/api/books/create', name: 'createBook', methods: ['POST'])]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour créer un livre')]
     public function createBook(EntityManagerInterface $em, Request $request, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository, ValidatorInterface $validator, TagAwareCacheInterface $cachePool): JsonResponse
@@ -112,6 +209,35 @@ class BookController extends AbstractController
         return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
+    /**
+     * Cette méthode permet de mettre à jour un livre en fonction de son id.
+     * 
+     *@OA\RequestBody(
+     *     description="Modifié un livre",
+     *     required=true,
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema (
+     *              type="object",
+     *              @OA\Property(property="title", description="Le titre du livre", type="string"),
+     *              @OA\Property(property="coverText", description="La couverture du livre", type="string"),
+     *              @OA\Property(property="idAuthor", description="Author", type="int"),
+     *              @OA\Property(property="comment", description="Le commentaire du bibliothéquaire", type="string")
+     *        )
+     *     )
+     *)
+     * 
+     * @OA\Response(
+     *     response=204,
+     *     description="Modifié un livre"
+     * ) 
+     *  
+     * @OA\Tag(name="Books")
+     * 
+     * @param Book $book
+     * @param EntityManagerInterface $em
+     * @return JsonResponse 
+     */
     #[Route('/api/books/edit/{id}', name: 'editBook', methods: ['PUT'])]
     public function editBook(EntityManagerInterface $em, Request $request, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository, Book $currentBook, TagAwareCacheInterface $cachePool, ValidatorInterface $validator): JsonResponse
     {
